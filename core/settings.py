2e29a55faecd1cd9d1fabc925a044c42ad2bdcd6
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2026-04-13 05:30:21 -0700

    webhooks

--- core/settings.py
+++ core/settings.py
@@ -235,3 +235,10 @@ MONNIFY_API_KEY = os.getenv('MONNIFY_API_KEY', 'test_api_key')
 MONNIFY_SECRET_KEY = os.getenv('MONNIFY_SECRET_KEY', 'test_secret_key')
 MONNIFY_BASE_URL = os.getenv('MONNIFY_BASE_URL', 'https://sandbox.monnify.com')
 MONNIFY_CONTRACT_CODE = os.getenv('MONNIFY_CONTRACT_CODE', '1234567890')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
